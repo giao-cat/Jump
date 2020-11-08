b0102066092d03f63abad6a21d558070f83a1c9e
giao-cat <73993547+giao-cat@users.noreply.github.com>
2020-11-08 18:02:53 +0800

    Add files via upload

---
+++ CameraControl.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    public Transform player;
+    
+
+    void Update()
+    {
+        transform.position = new Vector3 (0, player.position.y, -10f);
+    }
+}
---
+++ player.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+	 private Rigidbody2D rb;
+     private Animator anim;
+
+     public Collider2D coll;
+     public float speed;
+     public float jumpforce;
+     public LayerMask ground;
+   
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+		rb = GetComponent<Rigidbody2D>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		float horizontalmove = Input.GetAxis("Horizontal");
+        float facedircetion = Input.GetAxisRaw("Horizontal");
+		//角色移动
+        if (horizontalmove != 0)
+        {
+            rb.velocity = new Vector2(horizontalmove*speed * Time.deltaTime, rb.velocity.y);
+        }
+        if(facedircetion != 0)
+        {
+            transform.localScale = new Vector3(facedircetion, 1, 1);
+        }
+        
+    }
+}
